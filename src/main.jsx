c812069b2a41c488988a4d238ae6ab5e473d46ec
Michael Wood <michaelwood33311@icloud.com>
2023-06-18 12:09:04 -0400

    added Chakra Pro UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+/*
+ * Chakra Provider Setup: https://pro.chakra-ui.com/guides/get-started
+ */
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme as proTheme } from '@chakra-ui/pro-theme'
+import { extendTheme, theme as baseTheme, ColorModeScript } from '@chakra-ui/react'
+
+export const theme = extendTheme(
+  {
+    colors: { ...baseTheme.colors, brand: baseTheme.colors.messenger },
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+  proTheme
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>
 )
